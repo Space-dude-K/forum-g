c3a1c3d1cc2910afc05ffe4f17c1d32f5a9b1d37
Dude-sks <omicronyota@gmail.com>
2023-06-09 15:53:15 +0300

    Seeded system admin for auth service calls

--- Entities/Configuration/UserConfiguration.cs
+++ Entities/Configuration/UserConfiguration.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Entities.Models;
+using System.Reflection.Emit;
 
 namespace Entities.Configuration
 {
@@ -17,6 +18,9 @@ namespace Entities.Configuration
             builder.HasData(
             SeedUsers(5)
             );
+
+            //8e445865-a24d-4543-a6c6-9443d048cdb9
+            
         }
         private List<AppUser> SeedUsers(int usersCount)
         {
@@ -32,14 +36,41 @@ namespace Entities.Configuration
                     InternalPhone = Random.Shared.Next().ToString(),
                     BirthDate = RandomDay().ToString(),
                     UserName = "G600-U" + i,
+                    NormalizedUserName = ("G600-U" + i).ToUpper(),
                     Email = "G600-U" + i + "@mfrb.by",
+                    NormalizedEmail = ("G600-U" + i + "@mfrb.by").ToUpper(),
                     PhoneNumber = Random.Shared.Next().ToString() + Random.Shared.Next().ToString() + Random.Shared.Next().ToString()
                 };
 
-                users.Add(appUser);
+                //users.Add(appUser);
             }
 
+            users.Add(SeedSystemAdmin());
+
             return users;
+        }
+        // Admin - 1234567890
+        private AppUser SeedSystemAdmin()
+        {
+            AppUser appUser = new()
+            {
+                Id = "8e445865-a24d-4543-a6c6-9443d048cdb9",
+                FirstName = "System",
+                LastName = "Admin",
+                Cabinet = 0.ToString(),
+                InternalPhone = 0.ToString(),
+                BirthDate = 0.ToString(),
+                UserName = "Admin",
+                NormalizedUserName = "ADMIN",
+                Email = "Admin@admin.by",
+                NormalizedEmail = "ADMIN@ADMIN.BY",
+                PhoneNumber = 0.ToString(),
+                PasswordHash = "AQAAAAIAAYagAAAAEJmQy9UwxkODjbb/iQlo7ezznBC5omr0sEhFEoTgafpAxZZRFsyVCFG8NXKSc2SGJA=="
+                //SecurityStamp = "FGXU4FIM2LMJZFDJD3YCUQEHQRZY4GSS",
+                //ConcurrencyStamp = "b2394a5e-7d96-43c8-b354-c741d3f0733f"
+            };
+
+            return appUser;
         }
 
         private Random gen = new Random();
@@ -49,6 +80,5 @@ namespace Entities.Configuration
             int range = (DateTime.Today - start).Days;
             return start.AddDays(gen.Next(range));
         }
-
     }
 }
--- Entities/ForumContext.cs
+++ Entities/ForumContext.cs
@@ -2,6 +2,7 @@
 using Entities.Configuration.Forum;
 using Entities.Models;
 using Entities.Models.Forum;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -31,6 +32,20 @@ namespace Entities
 
             modelBuilder.ApplyConfiguration(new AccountConfiguration());
             modelBuilder.ApplyConfiguration(new ForumAccountTypeConfiguration());
+
+            // Seeding the relation between our Admin and roles to AspNetUserRoles table
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    RoleId = "1c5e174e-3b0e-446f-86af-483d56fd7210",
+                    UserId = "8e445865-a24d-4543-a6c6-9443d048cdb9"
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = "2c5e174e-3b0e-446f-86af-483d56fd7210",
+                    UserId = "8e445865-a24d-4543-a6c6-9443d048cdb9"
+                }
+            );
         }
         public DbSet<ForumUser> ForumUsers { get; set; }
         public DbSet<ForumCategory> ForumCategories { get; set; }
