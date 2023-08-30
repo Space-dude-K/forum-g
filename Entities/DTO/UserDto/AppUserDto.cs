5d2d4da89515b7d76fdd4ce7dab521bb5b358167
Space-dude-K <space.dude.work@gmail.com>
2023-08-30 17:22:23 +0300

    Post deletion access implementation

--- Entities/DTO/UserDto/AppUserDto.cs
+++ Entities/DTO/UserDto/AppUserDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Entities.DTO.UserDto
+﻿namespace Entities.DTO.UserDto
 {
     public class AppUserDto
     {
